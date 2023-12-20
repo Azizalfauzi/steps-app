3ba19f7f764eb7cb9f76874b40d8ffbcf789d78d
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-21 05:22:18 +0700

    *REACT STEP - useState example

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,23 +7,25 @@ const messages = [
 ];
 
 export default function App() {
-  const steps = 1;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    alert("Hello Previous");
+    if (step > 1) setStep(step - 1);
   }
+
   function handleNext() {
-    alert("Hello Next");
+    if (step < 3) setStep(step + 1);
   }
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${steps >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${steps >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${steps >= 3 ? "active" : ""}`}>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
       <p className="message">
-        Step {steps}:{messages[steps - 1]}
+        Step {step}:{messages[step - 1]}
       </p>
       <div className="buttons">
         <button
