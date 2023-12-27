b16d380438811153f3586de1f75ae606e5d48104
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-28 06:31:44 +0700

    *REACT STEPS - Reusable component button

--- src/App.js
+++ src/App.js
@@ -34,21 +34,32 @@ export default function App() {
             Step {step}:{messages[step - 1]}
           </p>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            <Button
+              textColor="#fff"
+              bgColor="#7950f2"
               onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              text="Previous"
+            />
+            <Button
+              textColor="#fff"
+              bgColor="#7950f2"
               onClick={handleNext}
-            >
-              Next
-            </button>
+              text="Next"
+            />
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, text }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
   );
 }
